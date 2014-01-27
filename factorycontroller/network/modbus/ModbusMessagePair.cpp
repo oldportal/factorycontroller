0634bcc08d36191031887c7ec56a26ee7eec19f0
Dmitry Ognyannikov <14166017+dmogn@users.noreply.github.com>
2014-01-27 02:16:31 -0200

    modbus protocol implementation

--- factorycontroller/network/modbus/ModbusMessagePair.cpp
+++ factorycontroller/network/modbus/ModbusMessagePair.cpp
@@ -38,6 +38,41 @@ oldportal::fc::network::modbus::ModbusMessagePair::ModbusMessagePair()
 }//END_ec0da4ab8c59963dd06b220481f2da85
 
 
+bool oldportal::fc::network::modbus::ModbusMessagePair::checkResponseCRC()
+{//BEGIN_6af82b8d8077d80fde78b23c5104b649
+    bool ret = false;
+    //TODO: checkResponseCRC()
+    return ret;
+}//END_6af82b8d8077d80fde78b23c5104b649
+
+uint8_t oldportal::fc::network::modbus::ModbusMessagePair::getResponseMessageLength()
+{//BEGIN_a3112b12fff85643440f7919f0706a64
+    uint8_t ret=0;
+    //TODO: getResponseMessageLength()
+    return ret;
+}//END_a3112b12fff85643440f7919f0706a64
+
+void oldportal::fc::network::modbus::ModbusMessagePair::setCRC(uint8_t message_length)
+{//BEGIN_4300ea1c4e1113846fd7fa4f2ad25c47
+    //TODO: setCRC()
+}//END_4300ea1c4e1113846fd7fa4f2ad25c47
+
+void oldportal::fc::network::modbus::ModbusMessagePair::setMessageAddress(uint16_t modbus_address)
+{//BEGIN_27d1c6b8262657603a141b32939a314e
+    //TODO: setMessageAddress()
+}//END_27d1c6b8262657603a141b32939a314e
+
+void oldportal::fc::network::modbus::ModbusMessagePair::setMessageFunction(uint8_t function_number)
+{//BEGIN_9dcb234106ede48c1d7609cfd0dd245d
+    //TODO: setMessageAddress()
+}//END_9dcb234106ede48c1d7609cfd0dd245d
+
+void oldportal::fc::network::modbus::ModbusMessagePair::setMessageLength(uint8_t message_length)
+{//BEGIN_1d7e8bfe0b992b38f28de1ac061a9ec1
+    //TODO: setCRC()
+}//END_1d7e8bfe0b992b38f28de1ac061a9ec1
+
+
 //BEGIN_USER_SECTION_AFTER_GENERATED_CODE
 
 //END_USER_SECTION_AFTER_GENERATED_CODE
