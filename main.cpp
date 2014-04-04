349ee48056227eaab277b5b5a1f07374e41610c2
Dmitry Ognyannikov <sirius_plus@yahoo.com>
2014-04-04 04:40:22 -0300

    command line parser header added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include "factorycontroller/factorycontroller.h"
 
+// command line argument parser
+#include <boost/program_options/cmdline.hpp>
+
 using namespace std;
 
 int main(int argc, char *argv[])
 {
+    //TODO: command line parser
+
     // init loader
     auto loader = std::make_shared<oldportal::fc::factory::manufacturing::proc::FactoryPureProgramLoader>();
     loader->init();
