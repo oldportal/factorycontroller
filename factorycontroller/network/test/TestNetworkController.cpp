00d811bed6f6e4be787ca57defeb8c42556bd421
Dmitry Ognyannikov <sirius_plus@yahoo.com>
2014-10-10 06:56:14 -0200

    test modbus connection class

--- factorycontroller/network/test/TestNetworkController.cpp
+++ factorycontroller/network/test/TestNetworkController.cpp
@@ -31,6 +31,21 @@
 //END_USER_SECTION_AFTER_MASTER_INCLUDE
 
 
+oldportal::fc::network::test::TestNetworkController::TestNetworkController(std::shared_ptr< oldportal::fc::network::Network > network)
+    : oldportal::fc::network::NetworkController(network)
+{//BEGIN_169badb0eee0d94f55cb0b833edc5814
+    assert(network && "ModbusNetworkController cannot be initialized with empty Network");
+
+    _run_thread_cycle_flag = false;
+    _close_interrupted_flag = false;
+    _network = network;
+
+    _modbus_ctx = nullptr;
+    _last_time_synchronization = std::chrono::high_resolution_clock::time_point::min();
+}//END_169badb0eee0d94f55cb0b833edc5814
+
+
+
 oldportal::fc::network::test::TestNetworkController::~TestNetworkController()
 {//BEGIN_76058ff9472a91dd56c649daea129d31
 
@@ -42,10 +57,26 @@ void oldportal::fc::network::test::TestNetworkController::close()
     // close() : empty
 }//END_82eae4bef3672100ed4503365393c1a3
 
+void oldportal::fc::network::test::TestNetworkController::closeModbusContext()
+{//BEGIN_29990b7655690fb97186e69254b261de
+    if (_modbus_ctx != nullptr)
+    {
+        modbus_close(_modbus_ctx);
+        modbus_free(_modbus_ctx);
+
+        _modbus_ctx = nullptr;
+    }
+}//END_29990b7655690fb97186e69254b261de
+
 void oldportal::fc::network::test::TestNetworkController::initHardware()
 {//BEGIN_303b6d9cb230b23ea149795bd0d06e69
     // initHardware() : empty
 }//END_303b6d9cb230b23ea149795bd0d06e69
+
+bool oldportal::fc::network::test::TestNetworkController::isOpened()
+{//BEGIN_c5fe45cf55b71530564330f8205cd654
+    return _modbus_ctx != nullptr && _realtime_thread;
+}//END_c5fe45cf55b71530564330f8205cd654
 
 void oldportal::fc::network::test::TestNetworkController::step()
 {//BEGIN_ae97a14dc042da1bef1d2d3f62dd7c88
