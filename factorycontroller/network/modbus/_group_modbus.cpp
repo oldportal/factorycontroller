75f70fb82aa0064f301b91c93f135b5a3c037cde
Dmitry Ognyannikov <14166017+dmogn@users.noreply.github.com>
2016-05-31 11:09:38 -0300

    license files added

--- factorycontroller/network/modbus/_group_modbus.cpp
+++ factorycontroller/network/modbus/_group_modbus.cpp
@@ -1,12 +1,12 @@
 /*
 *    This file is part of FactoryController project.
 *    
-*    factorycontroller is free software; you can redistribute it and/or modify
+*    FactoryController is free software; you can redistribute it and/or modify
 *    it under the terms of the GNU Lesser General Public License as published by
 *    the Free Software Foundation; either version 2 of the License, or
 *    (at your option) version 3.
 *    
-*    factorycontroller is distributed in the hope that it will be useful,
+*    FactoryController is distributed in the hope that it will be useful,
 *    but WITHOUT ANY WARRANTY; without even the implied warranty of
 *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 *    GNU Lesser General Public License for more details.
