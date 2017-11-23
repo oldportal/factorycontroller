25a35c52dfe303beb0310cb381aaed3153a4829b
Dmitry Ognyannikov <dmogn@mail.ru>
2017-11-23 15:02:04 -0300

    pugixml library added

--- util/serialization/_group_serialization.cpp
+++ util/serialization/_group_serialization.cpp
@@ -17,16 +17,14 @@
 
 std::vector< std::shared_ptr<oldportal::util::serialization::Serializable> > oldportal::util::serialization::load_from_xml(pugi::xml_node& archive)
 {//BEGIN_3c238bc26b07844b3c9d7b687712ba04
-    vector ret;
+    std::vector< std::shared_ptr<oldportal::util::serialization::Serializable> > ret;
     //TODO: load_archive()
     return ret;
 }//END_3c238bc26b07844b3c9d7b687712ba04
 
 void oldportal::util::serialization::save_to_xml(pugi::xml_node& archive, std::vector< std::shared_ptr<oldportal::util::serialization::Serializable> > root_objects)
 {//BEGIN_582086b231c70e77fc857ed3cc958d40
-    vector ret;
     //TODO: load_archive()
-    return ret;
 }//END_582086b231c70e77fc857ed3cc958d40
 
 
